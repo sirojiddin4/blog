4ce242b0fde60c57b2455d106ff7e144f399e8ca
Sirojiddin Bobokulov <109817510+sirojiddin4@users.noreply.github.com>
2023-05-11 20:58:24 +0200

    Delete Blog/blog directory

--- Blog/blog/main/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:id>/', views.post_detail, name='post_detail' ),
-
-]
